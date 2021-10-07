f4da243fe6c95b70e0df36449a16c62613ec2b88
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2021-10-07 12:25:19 +0530

    Create CircleArea.java

---
+++ CircleArea.java
@@ -0,0 +1,9 @@
+public class CircleArea{
+
+    public static void main(String[] args) {
+
+        double radius = Double.valueOf(args[0]);
+
+            System.out.println("Area of circle with radius " + radius + " is " + (radius  * radius  * 3.14));
+    }
+}
